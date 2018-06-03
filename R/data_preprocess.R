a28e5ed8b8ee28b643c3210c9a9bfbb59c161203
bAIo-lab <sarifi.statcomp@gmail.com>
2018-06-03 21:01:17 +0430

    data preprocess

---
+++ R/data_preprocess.R
@@ -0,0 +1,32 @@
+#' print
+#' @import softImptue
+#' @import ncImpute
+#' @import readxl
+#'
+#' @export
+#'
+
+data_preprocess <- function(data,
+                            impute = T,
+                            scale = T,
+                            is.cont = NULL) {
+  data <- data.matrix(data)
+  ## to do size of is.cont compatible with dim(data)[2]
+  if (length(is.cont) != 0) {
+    cont <- data[, which(is.cont)]
+    desc <- data[, !which(is.cont)]
+  } else{
+    level <- apply(data, 2 , function(x) length(unique(x)))
+    cont <- data[,which(level > 6)]
+    desc <- data[,which(level <= 6)]
+  }
+
+  if (impute) {
+    ncImpute(cont, rank=100, lambda = 30) -> cont
+   # softImpute(desc) -> desc
+  }
+  if (scale) {
+    cont <- biScale(cont)
+  }
+  cbind(cont, desc)
+}

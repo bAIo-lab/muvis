1e97e832dab74451a19a1b996d067c66bece2968
vdblm <vbalazadehmeresht@ce.sharif.edu>
2018-08-05 10:57:41 +0430

    ggm function added ... argument

--- R/ggm.R
+++ R/ggm.R
@@ -6,11 +6,9 @@
 #'
 #' @param  data A normalized dataframe or matrix with no missing data of continuous measurements.
 #' @param  methods (default = "glasso")
-#' @param  threshold A threshold for partial correlation thresholding method (default = 0.05). To be used only when the method "threshold" is used.
-#' @param  significance A cutoff for edge significance (default = 0.05). To be used only when the method "significance" is used.
-#' @param  rho (Non-negative) regularization parameter for glasso (default = 0.1). To be used only when the method "glasso" is used.
 #' @param  community (default = TRUE)
 #' @param  plot (default = FALSE)
+#' @param  ...
 #'
 #' @details The function combines the methods to construct the model, that is, the edge set is the intersection of all edge sets each of which is found by a method. The package gRim is used to implement AIC, BIC, and stepwise significance test. The method glasso from the package glasso is used to provide a sparse estimation of the inverse covariance matrix.
 #'
@@ -46,26 +44,38 @@
 
 ggm <-
   function(data,
-           threshold = 0.05,
-           significance = 0.05,
            methods = c("glasso"),
-           rho = 0.1,
            community = TRUE,
-           plot = FALSE) {
-    model <- gRim::cmod(~ . ^ ., data = data)
+           plot = FALSE,
+           ...) {
+    arguments <- list(...)
+    threshold <- arguments$threshold
+    significance <- arguments$significance
+    rho <- arguments$rho
+
+    if (is.null(threshold))
+      threshold <- 0.05
+
+    if (is.null(significance))
+      significance <- 0.05
+
+    if (is.null(rho))
+      rho = 0.1
+
+    model <- gRim::cmod( ~ . ^ ., data = data)
     S <- stats::cov.wt (data, method = "ML")$cov
     PC <- gRbase::cov2pcor(S)
     othermodels <- list()
-    if("aic" %in% methods){
+    if ("aic" %in% methods) {
       othermodels$aic <- aic <- gRbase::stepwise(model)
     }
-    if("bic" %in% methods){
+    if ("bic" %in% methods) {
       othermodels$bic <- gRbase::stepwise(model, k = log(nrow(data)))
     }
-    if("test" %in% methods){
+    if ("test" %in% methods) {
       othermodels$test <- gRbase::stepwise(model, criterion = "test")
     }
-    if("threshold" %in% methods){
+    if ("threshold" %in% methods) {
       Z <- abs(PC)
       Z[Z < threshold] <- 0
       diag(Z) <- 0
@@ -73,12 +83,13 @@ ggm <-
       g.thresh <-  methods::as(Z, "graphNEL")
       thresh <- gRim::cmod(g.thresh, data = data)
     }
-    if("sin" %in% methods){
+    if ("sin" %in% methods) {
       psin <- SIN::sinUG(S, n = nrow(data))
-      othermodels$gsin <- methods::as(SIN::getgraph(psin, significance), "graphNEL")
+      othermodels$gsin <-
+        methods::as(SIN::getgraph(psin, significance), "graphNEL")
     }
-    if("glasso" %in% methods){
-      C<-stats::cov2cor(S)
+    if ("glasso" %in% methods) {
+      C <- stats::cov2cor(S)
       res.lasso <- glasso::glasso(C, rho = rho)
       AM <- abs(res.lasso$wi) > threshold
       diag(AM) <- F
@@ -88,18 +99,11 @@ ggm <-
     }
     othermodels <- othermodels %>% purrr::map(methods::as, "igraph")
     commonedges <- do.call(igraph::intersection, othermodels)
-    graph.vis(commonedges, community = community, plot = plot, betweenness = T, directed=F)
-    # bt <- igraph::betweenness( methods::as(commonedges, "igraph"), igraph::V( methods::as(commonedges, "igraph")))
-    # data <- visNetwork::toVisNetworkData( methods::as(commonedges, "igraph"))
-    # if(community){
-    #   fc <- igraph::cluster_louvain( methods::as(commonedges, "igraph"))
-    #   data$nodes$group <- igraph::membership(fc)
-    # }
-    # vs <- visNetwork::visNetwork(nodes = data$nodes, edges = data$edges)  %>%
-    #   visNetwork::visOptions(highlightNearest = list(
-    #     enabled = TRUE,
-    #     degree = 1,
-    #     hover = TRUE
-    #   ))
-    # list(graph = as(commonedges, "igraph"), betweenness = bt, network = vs)
+    graph.vis(
+      commonedges,
+      community = community,
+      plot = plot,
+      betweenness = T,
+      directed = F
+    )
   }

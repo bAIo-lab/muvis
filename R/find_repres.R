187ba5cc557ae718704eea5471b618b1fa9b7ee1
vdblm <vbalazadehmeresht@ce.sharif.edu>
2018-10-07 18:32:29 +0330

    input graph instead of data

--- R/find_repres.R
+++ R/find_repres.R
@@ -2,19 +2,16 @@
 #'
 #'
 #' @description
-#' Uses \code{\link[muvis]{min_forest}} community detection and selects a number of variables from each community as representative.
+#' Uses \code{\link[muvis]{graph_vis}} community detection on the input graph and selects a number of variables from each community as representative.
 #'
 #'
-#' @param data A normalized dataframe or matrix with no missing data of continuous and (or) categorical measurements.
-#' @param ratio A number between (0, 1) indicating the ratio of data variables to be chosen as representative. Defaults to 0.1.
+#' @param graph An arbitrary graph object of data in R.
+#' @param ratio A number between (0, 1) indicating the ratio of variables to be chosen as representative. Defaults to 0.1.
 #' @param weighted A logical indicating if the number of nodes selected from each community is weighted. Defaults to FALSE. See details for more information.
-#' @param levels An integer value indicating the maximum number of levels of a categorical variable. To be used to distinguish the categorical variable.
-#' Defaults to NULL because it is supposed that \code{data} has been preprocessed using \code{\link[muvis]{data_preproc}} and the categorical variables are specified.
-#' If it is set, first will run \code{\link[muvis]{data_preproc}} to specify categorical and continuous variables.
 #'
 #'
 #' @details
-#' The function uses \code{\link[muvis]{min_forest}} to detect communities of variables. It sorts each community nodes based on their degree and selects an equal number of each based on \code{ratio} parameter.
+#' The function uses \code{\link[muvis]{graph_vis}} to detect communities of variables in the \code{graph} input. It sorts each community nodes based on their degree and selects an equal number of each based on \code{ratio} parameter.
 #' If \code{weighted} is TRUE it will select different number of nodes from each community based on their sizes.
 #'
 #'
@@ -23,13 +20,13 @@
 #'
 #' @examples
 #' data("NHANES")
-#' ## Using raw data
-#' repres_vars <- find_repres(data = NHANES[1:1000, ], ratio = .2, levels = 10)
+#' ## Using \code{\link[muvis]{min_forest}} graph
+#' mf <- min_forest(data = NHANES[1:1000, ], levels = 10)
+#' repres_vars <- find_repres(graph = mf$graph, ratio = .2)
 #'
-#' ## Using preprocessed data
-#' data <- data_preproc(NHANES, levels = 15)
-#' ## With \code{weighted = TRUE}
-#' repres_vars <- find_repres(data = data[1:1000, ], weighted = TRUE)
+#' ## Using \code{\link[muvis]{ggm}} graph with \code{weighted = TRUE}
+#' gm <- ggm(data = NHANES[1:1000, ], levels = 10)
+#' repres_vars <- find_repres(graph = gm$graph, weighted = TRUE)
 #'
 #' @return a vector containing names of representative variables.
 #'
@@ -39,24 +36,23 @@
 #' @importFrom igraph degree
 #'
 
-find_repres <- function (data,
+find_repres <- function (graph,
                          ratio = 0.1,
-                         weighted = FALSE,
-                         levels = NULL) {
-  mf <- min_forest(data, levels = levels, community = T)
+                         weighted = FALSE) {
+  gv <- graph_vis(graph, community = T)
   df <-
     data.frame(
-      node = mf$network$x$nodes$id,
-      community = mf$network$x$nodes$community,
-      degree = igraph::degree(mf$graph)[match(mf$network$x$nodes$id, names(igraph::degree(mf$graph)))]
+      node = gv$network$x$nodes$id,
+      community = gv$network$x$nodes$community,
+      degree = igraph::degree(gv$graph)[match(gv$network$x$nodes$id, names(igraph::degree(gv$graph)))]
     )
   df <- df[order(df$community,-df$degree),]
 
   community_lengths <-
     sapply(c(1:length(unique(
-      mf$communities
+      gv$communities
     ))), function(x)
-      sum(mf$communities == x))
+      sum(gv$communities == x))
   if (!weighted)
     weights <-
     rep(1 / length(community_lengths), length(community_lengths))
